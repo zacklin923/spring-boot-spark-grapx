aea919402651b2d8cfa45a4852ee3aae84aa0186
liping@mininglamp.com <liping@mininglamp.com>
2018-04-11 21:40:16 +0800

    初始化springboot工程

---
+++ src/main/java/com/mininglamp/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.mininglamp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * description:
+ * author: liping@mininglamp.com
+ * date: ${DATE}
+ * package: ${PACKAGE_NAME}
+ * 测试用controller
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello, World";
+    }
+}
---
+++ src/main/java/com/mininglamp/main/AntiMoneyLaunderingApplication.java
@@ -0,0 +1,15 @@
+package com.mininglamp.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序入口
+ */
+@SpringBootApplication
+public class AntiMoneyLaunderingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntiMoneyLaunderingApplication.class, args);
+    }
+}
