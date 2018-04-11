aed1e07527a8cdc9acbadcb8848736d9924d7439
liping@mininglamp.com <liping@mininglamp.com>
2018-04-11 21:53:21 +0800

    初版问题调整（启动后访问/hello报错404），添加包扫描后ok

--- src/main/java/com/mininglamp/main/AntiMoneyLaunderingApplication.java
+++ src/main/java/com/mininglamp/main/AntiMoneyLaunderingApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * 程序入口
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.mininglamp")
 public class AntiMoneyLaunderingApplication {
 
     public static void main(String[] args) {
